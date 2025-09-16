cf7bd671868a3c9701abb06ef29371e20c1df4aa
Lee, juhyung <p99265407@gmail.com>
2025-09-16 20:38:02 +0900

    five

--- World.java
+++ World.java
@@ -3,4 +3,9 @@ public class World {
     public static void run() {
         System.out.println("202214062");
     }
+    
+    public static void run2() {
+        System.out.println("hello world");
+    }
+
 }
